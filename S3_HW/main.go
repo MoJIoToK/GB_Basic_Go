be5b35fafc07e6fcf469007b0b01905b30a511ee
Nick <mojiotok167@gmail.com>
2024-03-19 19:08:38 +0300

    update hw for seminar 3

--- S3_HW/main.go
+++ S3_HW/main.go
@@ -5,41 +5,47 @@ import (
 	"math"
 )
 
-type Shape struct {
-	Name string
+type Name string
+
+type Shape interface {
+	GetArea() float64
+	GetName() string
 }
 
 type Rectangle struct {
-	Shape
+	name   string
 	Length float64
 	Width  float64
 }
 
 type Circle struct {
-	Shape
+	name   string
 	Radius float64
 }
 
-func (c Circle) Area() float64 {
+func (c Circle) GetArea() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func (r Rectangle) Area() float64 {
+func (c Circle) GetName() string {
+	c.name = "Круг"
+	return c.name
+}
+
+func (r Rectangle) GetArea() float64 {
 	return r.Width * r.Length
 }
 
-func (s Shape) GetName() string {
-	return s.Name
-}
-
-func (s Shape) Area() float64 {
-	return 0.0
+func (r Rectangle) GetName() string {
+	r.name = "Прямоугольник"
+	return r.name
 }
 
 func main() {
-	circle := Circle{Shape{"Круг"}, 5}
-	rectangle := Rectangle{Shape{"Прямоугольник"}, 4, 6}
+	circle := Circle{Radius: 5}
+	//circle := Circle{Shape{"Круг"}, 5}
+	//rectangle := Rectangle{Shape{"Прямоугольник"}, 4, 6}
 
-	fmt.Printf("%s: Площадь = %.2f\n", circle.GetName(), circle.Area())
-	fmt.Printf("%s: Площадь = %.2f\n", rectangle.GetName(), rectangle.Area())
+	fmt.Printf("%s: Площадь = %.2f\n", circle.GetName(), circle.GetArea())
+	//fmt.Printf("%s: Площадь = %.2f\n", rectangle.GetName(), rectangle.GetArea())
 }
